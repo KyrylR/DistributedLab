e648994e83695991584bb814e4512e80a112d93e
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-06-21 11:21:53 +0300

    AES tests fixs

--- CryptographicAlgorithms/AES/test_AES.py
+++ CryptographicAlgorithms/AES/test_AES.py
@@ -9,14 +9,15 @@ class AESTestCase(unittest.TestCase):
 
     def test_encryption(self):
         plaintext = 0x3243f6a8885a308d313198a2e0370734
+        print(f"Plain text: {plaintext}")
         encrypted = self.AES.encrypt(plaintext)
-
+        print(f"Encrypted text: {encrypted}")
         self.assertEqual(encrypted, 0x3925841d02dc09fbdc118597196a0b32)
 
     def test_decryption(self):
         ciphertext = 0x3925841d02dc09fbdc118597196a0b32
         decrypted = self.AES.decrypt(ciphertext)
-
+        print(f"Decrypted text: {decrypted}")
         self.assertEqual(decrypted, 0x3243f6a8885a308d313198a2e0370734)
 
 
